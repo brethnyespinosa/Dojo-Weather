1db2f797737f8534884d89238eb902665ae48da6
Brethny Espinosa <brethnyespinosa@gmail.com>
2021-08-24 15:41:19 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function hide(element){
+element.remove();
+}
+
+var temp = document.querySelector("#tempchange");
+var temp1 = document.querySelector("#tempchange1");
+var temp2 = document.querySelector("#tempchange2");
+var temp3 = document.querySelector("#tempchange3");
+var temp4 = document.querySelector("#tempchange4");
+var temp5 = document.querySelector("#tempchange5");
+var temp6 = document.querySelector("#tempchange6");
+var temp7 = document.querySelector("#tempchange7");
+
+function degreesftoc(){
+    temp.innerText = "-4.4°";
+    temp1.innerText = "-7°";
+    temp2.innerText = "-2.7°";
+    temp3.innerText = "-7.2°";
+    temp4.innerText = "-6.1°";
+    temp5.innerText = "-8.8°";
+    temp6.innerText = "-3.3°";
+    temp7.innerText = "-6.1°";
+
+}
